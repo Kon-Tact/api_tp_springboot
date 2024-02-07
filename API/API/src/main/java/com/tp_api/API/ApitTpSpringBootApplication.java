e450e641760a11322f3823ecd495c8d92238e212
Mathias <brenzokuken@gmail.com>
2024-02-07 15:58:00 +0100

    first commit

---
+++ API/API/src/main/java/com/tp_api/API/ApitTpSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.tp_api.API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApitTpSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApitTpSpringBootApplication.class, args);
+	}
+
+}
